aff33c3aaac690077a2415395a45fae2674b7089
lucas-ssv <lucas_99silva@hotmail.com>
2021-11-18 16:43:10 -0300

    fix: add linter to the files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -3,8 +3,8 @@ import { RemoteAuthentication } from './remote-authentication'
 import faker from 'faker'
 
 type SutTypes = {
-  sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostClientSpy;
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
@@ -20,7 +20,7 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     const url = faker.internet.url()
-    const { sut, httpPostClientSpy } = makeSut(url);
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
 
     expect(httpPostClientSpy.url).toBe(url)
