95f29305cd7b0f68869913df3537bc97091c72eb
lucas-ssv <lucas_99silva@hotmail.com>
2021-12-03 17:43:52 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/Login/Login.spec.tsx
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -58,6 +58,18 @@ describe('Login component', () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
+  test('Should show valid email state if validation succeeds', () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() }
+    })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('tudo certo!')
+    expect(emailStatus.textContent).toBe('🔵')
+  })
+
   test('Should show valid password state if validation succeeds', () => {
     const { sut, validationSpy } = makeSut()
     validationSpy.errorMessage = null
