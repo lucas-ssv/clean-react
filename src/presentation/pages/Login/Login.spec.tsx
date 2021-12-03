3cef8f8384bd975afc09b5804c458aae723e9075
lucas-ssv <lucas_99silva@hotmail.com>
2021-12-03 17:42:18 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/Login/Login.spec.tsx
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -57,4 +57,16 @@ describe('Login component', () => {
     expect(passwordStatus.title).toBe(validationSpy.errorMessage)
     expect(passwordStatus.textContent).toBe('🔴')
   })
+
+  test('Should show valid password state if validation succeeds', () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() }
+    })
+    const passwordStatus = sut.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('tudo certo!')
+    expect(passwordStatus.textContent).toBe('🔵')
+  })
 })
