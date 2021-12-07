c622a51c699a09f43e34059ca319898ddbd124e7
lucas-ssv <lucas_99silva@hotmail.com>
2021-12-07 10:59:05 -0300

    fix: change toEqual to toStrictEqual

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -6,7 +6,7 @@ describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const field = faker.database.column()
     const validations = sut.field(field).required().build()
-    expect(validations).toEqual([new RequiredFieldValidation(field)])
+    expect(validations).toStrictEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
