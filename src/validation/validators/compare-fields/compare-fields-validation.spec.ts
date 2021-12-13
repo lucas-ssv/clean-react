ffe54f3217d30a3f47926cd2a01c772744ae7441
lucas-ssv <lucas_99silva@hotmail.com>
2021-12-13 14:56:55 -0300

    test: ensure CompareFieldsValidation returns falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -8,4 +8,11 @@ describe('CompareFieldsValidation', () => {
     const error = sut.validate('another_field')
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  test('Should return falsy if compare is valid', () => {
+    const valueToCompare = 'any_value'
+    const sut = new CompareFieldsValidation('any_field', valueToCompare)
+    const error = sut.validate(valueToCompare)
+    expect(error).toBeFalsy()
+  })
 })
