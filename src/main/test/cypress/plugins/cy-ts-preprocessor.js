e29b1de4d2450bd1b3d1c41f8bb13cfdfb984398
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-16 22:24:21 -0300

    refactor: move duplicated code to a helper methods

--- src/main/test/cypress/plugins/cy-ts-preprocessor.js
+++ src/main/test/cypress/plugins/cy-ts-preprocessor.js
@@ -1,17 +1,16 @@
-// Copy of https://github.com/bahmutov/cypress-angular-unit-test/blob/master/cypress/plugins/cy-ts-preprocessor.js
 const wp = require('@cypress/webpack-preprocessor')
 
 module.exports = wp({
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
+  webpackOptions: {
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [{
         test: /\.ts$/,
         exclude: /node_modules/,
-        loaders: 'ts-loader'
-      }
-    ]
+        loader: 'ts-loader'
+      }]
+    }
   }
 })
---
+++ src/main/test/cypress/support/http-mocks.ts
@@ -0,0 +1,20 @@
+import { Method } from 'axios'
+
+export const mockInvalidCredentialsError = (url: RegExp): void => {
+  cy.intercept('POST', url, {
+    statusCode: 401
+  }).as('request')
+}
+
+export const mockUnexpectedError = (url: RegExp, method: Method): void => {
+  cy.intercept(method, url, {
+    statusCode: 400
+  }).as('request')
+}
+
+export const mockOk = (url: RegExp, method: Method, body: any): void => {
+  cy.intercept(method, url, {
+    statusCode: 200,
+    body
+  }).as('request')
+}
