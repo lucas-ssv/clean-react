2c61861a8b3afaab5cc3b99e780c4e8ee0abf9e2
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-17 22:42:00 -0300

    test: ensure RemoteLoadSurveyList calls HttpGetClient with correct url

---
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -0,0 +1,13 @@
+import { HttpGetClientMock } from '@/data/test'
+import { RemoteLoadSurveyList } from './remote-load-survey-list'
+import faker from 'faker'
+
+describe('RemoteLoadSurveyList', () => {
+  test('Should call HttpGetClient with correct url', async () => {
+    const url = faker.internet.url()
+    const httpGetClientSpy = new HttpGetClientMock()
+    const sut = new RemoteLoadSurveyList(url, httpGetClientSpy)
+    await sut.loadAll()
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+})
