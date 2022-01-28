2d62ec26c8f11d4e6326c27d9db34d0bc593b9b9
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-28 17:55:50 -0300

    refactor: change asserts to use jest-dom

--- src/presentation/test/form-helper.tsx
+++ src/presentation/test/form-helper.tsx
@@ -1,23 +1,13 @@
 import { fireEvent, screen } from '@testing-library/react'
 import faker from 'faker'
 
-export const testChildCount = (fieldName: string, count: number): void => {
-  const el = screen.getByTestId(fieldName)
-  expect(el.childElementCount).toBe(count)
-}
-
-export const testButtonIsDisabled = (fieldName: string, isDisabled: boolean): void => {
-  const el = screen.getByTestId(fieldName) as HTMLButtonElement
-  expect(el.disabled).toBe(isDisabled)
-}
-
 export const testStatusForField = (fieldName: string, validationError: string = ''): void => {
   const wrap = screen.getByTestId(`${fieldName}-wrap`)
   const field = screen.getByTestId(fieldName)
   const label = screen.getByTestId(`${fieldName}-label`)
-  expect(wrap.getAttribute('data-status')).toBe(validationError ? 'invalid' : 'valid')
-  expect(field.title).toBe(validationError)
-  expect(label.title).toBe(validationError)
+  expect(wrap).toHaveAttribute('data-status', validationError ? 'invalid' : 'valid')
+  expect(field).toHaveProperty('title', validationError)
+  expect(label).toHaveProperty('title', validationError)
 }
 
 export const populateField = (fieldName: string, value = faker.random.word()): void => {
@@ -25,14 +15,4 @@ export const populateField = (fieldName: string, value = faker.random.word()): v
   fireEvent.input(input, {
     target: { value }
   })
-}
-
-export const testElementExists = (fieldName: string): void => {
-  const el = screen.getByTestId(fieldName)
-  expect(el).toBeTruthy()
-}
-
-export const testElementText = (fieldName: string, text: string): void => {
-  const el = screen.getByTestId(fieldName)
-  expect(el.textContent).toBe(text)
 }
