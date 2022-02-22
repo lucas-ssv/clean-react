746cda6861c0e0f0ac9aedb382f663308af70fea
lucas-ssv <lucas_99silva@hotmail.com>
2022-02-22 16:11:04 -0300

    refactor: move SurveyResultData to its own component

--- src/presentation/pages/SurveyResult/SurveyResult.tsx
+++ src/presentation/pages/SurveyResult/SurveyResult.tsx
@@ -1,10 +1,10 @@
-import { Calendar, Footer, Header, Loading, Error } from '@/presentation/components'
+import { Footer, Header, Loading, Error } from '@/presentation/components'
 import Styles from './survey-result-styles.scss'
-import FlipMove from 'react-flip-move'
 import React, { useEffect, useState } from 'react'
 import { LoadSurveyResult } from '@/domain/usecases'
 import { useErrorHandler } from '@/presentation/hooks'
 import { useNavigate } from 'react-router-dom'
+import { Result } from './components'
 
 type Props = {
   loadSurveyResult: LoadSurveyResult
@@ -36,24 +36,7 @@ export const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div data-testid="survey-result" className={Styles.contentWrap}>
-        {state.surveyResult &&
-          <>
-            <hgroup>
-              <Calendar date={state.surveyResult.date} className={Styles.calendarWrap} />
-              <h2 data-testid="question">{state.surveyResult.question}</h2>
-            </hgroup>
-            <FlipMove data-testid="answers" className={Styles.answersList}>
-              {state.surveyResult.answers.map(answer => (
-                <li data-testid="answer-wrap" className={answer.isCurrentAccountAnswer ? Styles.active : ''} key={answer.answer}>
-                  {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
-                  <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
-                  <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
-                </li>
-              ))}
-            </FlipMove>
-            <button data-testid="back-button" onClick={() => history('/')}>Voltar</button>
-          </>
-        }
+        {state.surveyResult && <Result surveyResult={state.surveyResult} />}
         {state.isLoading && <Loading />}
         {state.error && <Error error={state.error} reload={reload} />}
       </div>
