51b76b6f9bd34d77073235c4d793c1e95f62387f
lucas-ssv <lucas_99silva@hotmail.com>
2022-02-23 11:20:32 -0300

    test: ensure RemoteSaveSurveyResult returns UnexpectedError on 404

--- src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -3,7 +3,7 @@ import { HttpStatusCode } from '@/data/protocols/http'
 import { RemoteSaveSurveyResult } from './remote-save-survey-result'
 import faker from 'faker'
 import { mockSaveSurveyResultParams } from '@/domain/test'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteSaveSurveyResult
@@ -42,5 +42,14 @@ describe('RemoteSaveSurveyResult', () => {
     }
     const promise = sut.save(mockSaveSurveyResultParams())
     expect(promise).rejects.toThrow(new AccessDeniedError())
+  })
+
+  test('Should throw UnexpectedError if HttpClient returns 404', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.save(mockSaveSurveyResultParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
