c1dbb91afac7f789d223a096d263decac8a38b09
lucas-ssv <lucas_99silva@hotmail.com>
2022-02-25 15:53:35 -0300

    test: ensure SurveyResult present error on SaveSurveyResult error

--- src/main/test/cypress/integration/SurveyResult.spec.ts
+++ src/main/test/cypress/integration/SurveyResult.spec.ts
@@ -2,59 +2,80 @@ import * as Helper from '../utils/helpers'
 import * as Http from '../utils/http-mocks'
 
 const path = /surveys/
-const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
-const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
-const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-result')
+const mockLoadSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-result')
 
 describe('SurveyResult', () => {
-  beforeEach(() => {
-    cy.fixture('account').then(account => {
-      Helper.setLocalStorageItem('account', account)
+  describe('load', () => {
+    const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+    const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
+
+    beforeEach(() => {
+      cy.fixture('account').then(account => {
+        Helper.setLocalStorageItem('account', account)
+      })
+      cy.visit('/surveys/any_id')
     })
-    cy.visit('/surveys/any_id')
-  })
 
-  it('Should present error on UnexpectedError', () => {
-    mockUnexpectedError()
-    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
-  })
-
-  it('Should reload on button click', () => {
-    mockUnexpectedError()
-    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
-    mockSuccess()
-    cy.getByTestId('reload').click()
-    cy.getByTestId('question').should('exist')
-  })
-
-  it('Should logout on AccessDeniedError', () => {
-    mockAccessDeniedError()
-    Helper.testUrl('/login')
-  })
-
-  it('Should present survey result', () => {
-    mockSuccess()
-    cy.getByTestId('question').should('have.text', 'Question')
-    cy.getByTestId('day').should('have.text', '03')
-    cy.getByTestId('month').should('have.text', 'fev')
-    cy.getByTestId('year').should('have.text', '2018')
-    cy.get('li:nth-child(1)').then(li => {
-      assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
-      assert.equal(li.find('[data-testid="image"]').attr('src'), 'any_image')
-      assert.equal(li.find('[data-testid="percent"]').text(), '70%')
+    it('Should present error on UnexpectedError', () => {
+      mockUnexpectedError()
+      cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     })
-    cy.get('li:nth-child(2)').then(li => {
-      assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer_2')
-      assert.notExists(li.find('[data-testid="image"]'))
-      assert.equal(li.find('[data-testid="percent"]').text(), '30%')
+
+    it('Should reload on button click', () => {
+      mockUnexpectedError()
+      cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+      mockLoadSuccess()
+      cy.getByTestId('reload').click()
+      cy.getByTestId('question').should('exist')
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      Helper.testUrl('/login')
+    })
+
+    it('Should present survey result', () => {
+      mockLoadSuccess()
+      cy.getByTestId('question').should('have.text', 'Question')
+      cy.getByTestId('day').should('have.text', '03')
+      cy.getByTestId('month').should('have.text', 'fev')
+      cy.getByTestId('year').should('have.text', '2018')
+      cy.get('li:nth-child(1)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
+        assert.equal(li.find('[data-testid="image"]').attr('src'), 'any_image')
+        assert.equal(li.find('[data-testid="percent"]').text(), '70%')
+      })
+      cy.get('li:nth-child(2)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer_2')
+        assert.notExists(li.find('[data-testid="image"]'))
+        assert.equal(li.find('[data-testid="percent"]').text(), '30%')
+      })
+    })
+
+    it('Should go to SurveyList on back button click', () => {
+      cy.visit('')
+      mockLoadSuccess()
+      cy.visit('/surveys/any_id')
+      cy.getByTestId('back-button').click()
+      Helper.testUrl('/')
     })
   })
 
-  it('Should go to SurveyList on back button click', () => {
-    cy.visit('')
-    mockSuccess()
-    cy.visit('/surveys/any_id')
-    cy.getByTestId('back-button').click()
-    Helper.testUrl('/')
+  describe('save', () => {
+    const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+
+    beforeEach(() => {
+      cy.fixture('account').then(account => {
+        Helper.setLocalStorageItem('account', account)
+      })
+      mockLoadSuccess()
+      cy.visit('surveys/any_id')
+    })
+
+    it('Should present error on UnexpectedError', () => {
+      mockUnexpectedError()
+      cy.get('li:nth-child(2)').click()
+      cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    })
   })
 })
